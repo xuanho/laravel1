e071687fdf17eedfc716d0ce3cfc81c87d5e5f6a
Xuan Ho <xuan@javactechnology.com>
2025-06-15 15:01:58 +0700

    Laravel – Eloquent

--- routes/web.php
+++ routes/web.php
@@ -7,16 +7,21 @@ use App\Models\Job;
 Route::get('/', function () {
     return view('home');
 });
+Route::get('/jobs/create', function(){
+    return view('jobs.create');
+});
 Route::get('/jobs', function () {
     // $job = Job::all(); // lazy loading
     $job = Job::with('employer')->paginate(10); // eager loading
     // $job = Job::with('employer')->cursorPaginate(5); // eager loading
-    return view('jobs', ['jobs' => $job]);
+    return view('jobs.index', ['jobs' => $job]);
 });
 Route::get('/jobs/{id}', function ($id) {
-    return view('job', ['job' => Job::find($id)]);
+    return view('jobs.show', ['job' => Job::find($id)]);
 });
 Route::get('/contact', function () {
     return view('contact');
 });
- 
+ Route::post('/jobs', function(){
+    dd('test');
+ });

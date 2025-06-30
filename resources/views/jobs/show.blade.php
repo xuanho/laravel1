843eec406a8d1525a67c04b9e95b294f71b83197
Xuan Ho <xuan@javactechnology.com>
2025-06-30 08:53:29 +0700

    editing

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -4,5 +4,10 @@
     </x-slot:heading>
    <h2><strong> {{ $job['title'] }}</strong></h2>
     <p>{{$job['company']}} at {{$job['location']}}</p>
+    <p class="mt-6">
+        <x-button href="/jobs">
+            Edit Job
+        </x-button>
+    </p>
    
 </x-layout>
